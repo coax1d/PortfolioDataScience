6d8998352551c1f26974e9fec67fc81a51dedb87
coax1d <andrewburger1991@gmail.com>
2018-05-10 19:53:05 -0700

    Update SpamFilterWithValidation.py

--- dataScienceRepo/SpamFilterWithValidation.py
+++ dataScienceRepo/SpamFilterWithValidation.py
@@ -9,23 +9,25 @@ Created on Sat Jan 13 21:08:44 2018
 
 from sklearn.naive_bayes import GaussianNB
 import sklearn.model_selection as skl
-import pandas as pd
+import numpy as np
+from numpy import genfromtxt
 
-gnb = GaussianNB()
+
 
 # Using Naive Base to classify either Spam or Ham Then validating and reporting the 5-fold cross val error
 
+# Getting data organized
+my_data = genfromtxt("C:\\Users\\Elena Burger\\Downloads\\spambase.csv", delimiter=',') #get data
 
-Data = pd.read_csv("C:\\Users\\Public\\ECE523\\Data\\CSV\\spambase.csv")
-numPyData = Data.values
-
-X_Data = numPyData[:, :-1].copy()
-y_label = numPyData[:,-1].copy()
+X_Data = my_data[:, :-1].copy() 
+y_label = my_data[:,-1].copy()
 
 
-output = skl.cross_val_score(gnb, X_Data, y= y_label)
+gnb = GaussianNB()
+output = skl.cross_val_score(gnb, X_Data, y= y_label, cv=5)
+mean = np.mean(output)
 
-print(output)
+print(mean)
 
 
 
